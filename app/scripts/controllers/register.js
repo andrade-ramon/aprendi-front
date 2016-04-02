20454d40ab799440905ed2449a05eee926ca0786
Ramon Andrade <andrade.rmn@gmail.com>
2016-04-02 18:41:25 -0300

    Corrige modelo de dados enviados para registro

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -1,13 +1,12 @@
 app.controller('RegisterCtrl', function ($scope, $http, $localStorage, $location, ENV) {
 
-	$scope.student = {};
+	$scope.user = {};
 
 	$scope.register = function() {
-	    $http.post(ENV.API.REGISTER, $scope.student).then(function(response) {
+	    $http.post(ENV.API.REGISTER, $scope.user).then(function(response) {
 	        $localStorage.token = response.data.token;
             $location.path('/');
 	    }, function(response) {
-			console.log(response);
 	    	$scope.msg = response.data.message;
 	    });
 	};
