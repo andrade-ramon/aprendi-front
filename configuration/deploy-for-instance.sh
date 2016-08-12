f1f51d1bda30ade2cb54b878dddd548ff4ad67ff
Eric Dallo <ericdallo06@hotmail.com>
2016-08-12 19:52:40 -0300

    Corrige permissao nos arquivos

--- configuration/deploy-for-instance.sh
+++ configuration/deploy-for-instance.sh
@@ -7,16 +7,21 @@ ZEUS_TEMP_DIR=/tmp/zeus
 ZEUS_PROD_DIR=/opt/app
 
 git clone https://github.com/qualfacul/zeus.git $ZEUS_TEMP_DIR
+
+mkdir $ZEUS_TEMP_DIR/bucket
+gcsfuse qual-facul.appspot.com $ZEUS_TEMP_DIR/bucket
+
+cp $ZEUS_TEMP_DIR/bucket/zeus/production.js $ZEUS_TEMP_DIR/app/scripts/properties.js
+fusermount -u $ZEUS_TEMP_DIR/bucket
+rm -r $ZEUS_TEMP_DIR/bucket
+
 cd $ZEUS_TEMP_DIR
-
 npm install
-cp /mnt/bucket/zeus/production.js $ZEUS_TEMP_DIR/app/scripts/properties.js
-
 bower install --allow-root --config.interactive=false
 grunt build
 
 mv $ZEUS_TEMP_DIR/bower_components $ZEUS_TEMP_DIR/app
 
-mv $ZEUS_PROD_DIR $ZEUS_PROD_DIR.tmp
-mv $ZEUS_TEMP_DIR $ZEUS_PROD_DIR
-rm -rf $ZEUS_PROD_DIR.tmp
+sudo mv $ZEUS_PROD_DIR $ZEUS_PROD_DIR.tmp
+sudo mv $ZEUS_TEMP_DIR $ZEUS_PROD_DIR
+sudo rm -rf $ZEUS_PROD_DIR.tmp
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -ve
-gcloud compute ssh zeus --command /opt/app/configuration/deploy-for-instance.sh
+gcloud compute ssh zeus --zone us-east1-b --command /opt/app/configuration/deploy-for-instance.sh
