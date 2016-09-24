bc002080772c7e801ea3c29f533e102b83abf3c4
Eric Dallo <ericdallo06@hotmail.com>
2016-09-23 21:13:35 -0300

    Corrige layout de comentarios

--- app/scripts/controllers/college_profile.js
+++ app/scripts/controllers/college_profile.js
@@ -32,14 +32,6 @@ app.controller('CollegeProfileCtrl', function (User, $scope, $http, $route, $rou
         {createdAt: "30min", content: "In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam id dolor. Class aptent taciti sociosqu ad litora In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, "}
     ];
 
-    $http({
-        url: ENV.API.COLLEGE.COMMENTS.ALL.replace(ENV.ARG1, collegeId)
-    }).then(function success(response) {
-        $scope.timeline.conversations = response.data;
-    }, function error() {
-        $scope.timeline.conversations = [];
-    });
-    
     $scope.courses = [{id: 123, name: "NOME DO CURSO"}, {id: 456, name: "OUTRO CURSO"}];
 
     $scope.listCampus = function () {
@@ -48,14 +40,26 @@ app.controller('CollegeProfileCtrl', function (User, $scope, $http, $route, $rou
         });
     };
 
+    var listComments = function() {
+        $http({
+            url: ENV.API.COLLEGE.COMMENTS.ALL.replace(ENV.ARG1, collegeId)
+        }).then(function success(response) {
+            $scope.timeline.conversations = response.data;
+        }, function error() {
+            $scope.timeline.conversations = [];
+        });
+    };
+
+    listComments();
+
     $scope.leaveComment = function () {
         $http({
             method: 'POST',
             url: ENV.API.COLLEGE.COMMENTS.NEW.replace(ENV.ARG1, collegeId),
             data: {'messateText': $scope.comment.text}
         }).then(function success() {
-            messagesContainer.addSuccess("Aguarde a faculdade responder o comentario");
-            $route.reload();
+            listComments();
+            $('#comments-tab').attr('checked', true);
         }, function error() {
             messagesContainer.addError("Não foi possivel deixar um comentário, tente mais tarde");
             $route.reload();
@@ -87,8 +91,7 @@ app.controller('CollegeProfileCtrl', function (User, $scope, $http, $route, $rou
             url: ENV.API.COLLEGE.COMMENTS.REPLY.replace(ENV.ARG1, collegeId).replace(ENV.ARG2, conversationId),
             data: {'messateText': $scope.reply.text}
         }).then(function success() {
-            messagesContainer.addSuccess("Comentario respondido !");
-            $route.reload();
+            listComments();
         }, function error() {
             messagesContainer.addError("Não foi possivel responder um comentário, tente mais tarde");
             $route.reload();
