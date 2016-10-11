4f55e2a49ad81cd566042667d16def36e7fef78c
Eric Dallo <ericdallo06@hotmail.com>
2016-10-11 01:06:53 -0300

    Corrige headermenu

--- app/scripts/controllers/header_menu.js
+++ app/scripts/controllers/header_menu.js
@@ -1,5 +1,6 @@
 app.controller('HeaderMenuCtrl', function ($scope, $location) {
 	$scope.search = {};
+	$scope.loginUrl = '/login?redirectAfterLogin=' + $location.path();
 
     $scope.search = function() {
         $location.path('/faculdades/pesquisa/' + $scope.search.query);
